6152fbb9ffebdd64eab6b01aaff60ea0ac7aebe7
akazachkov <akazachkov@ya.ru>
2026-01-21 11:55:40 +0300

    Косметическая правка конфига

--- config/app_config.py
+++ config/app_config.py
@@ -3,19 +3,21 @@
 from pathlib import Path
 
 
-# Определяем корневую директорию проекта.
+# Определяем корневую директорию проекта и путь к папке с модулями.
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 MODULES_DIR = PROJECT_ROOT / "modules"
+
+# Путь к файлу с конфигурацией.
 CONFIG_PATHS_NAME = PROJECT_ROOT / "config" / "config_paths_name.toml"
 
-# Название приложения, логотив в строке заголовка.
-APP_TITLE = "The F.I.R. 2.0a (File Interaction Runner)"
+# Название приложения, логотип в строке заголовка.
+APP_TITLE = "The F.I.R. v.2 (File Interaction Runner)"
 APP_LOGO = PROJECT_ROOT / "images" / "logo_small.png"
 
-# Определяем геометрию приложения.
+# Геометрия приложения.
 GEOMETRY_MAIN_WINDOW = "700x300"  # Начальный/минимальный размер окна.
 MAIN_WINDOW_MAXSIZE = "1500x900"  # Максимальный размер окна.
 WRAPLENGHT_BUTTON = 200  # Максимальная ширина лейбла, над кнопкой модуля.
 
-# Определяем, сколько подключаемых модулей может быть открыто одновременно.
+# Количество одновременно работающих подключаемых модулей.
 MAX_CONCURRENT_MODULES = 3
